f8f6f71519faa2d52ce65a273a0f434cbfb0a4b1
s-ong-c <somony9292@gmail.com>
2020-07-09 13:24:35 +0900

    gh-pages test

--- packages/customHooks/index.ts
+++
@@ -1,6 +0,0 @@
-import { MutableRefObject } from 'react';
-import actionHook from './lib/actionHook';
-
-export const useHover = actionHook({
-  events: ['mouseenter', 'mouseleave'],
-}) as <T>(ref: MutableRefObject<T>) => boolean;
--- packages/docs/src/Components/Button/examples/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as color } from './Color';
-export { default as disabled } from './Disabled';
